99ae6590e49c23d33fc815e1178b85f810679999
msaranya29 <smalways123@gmail.com>
2025-08-17 10:19:25 +0530

    Queue with Array

---
+++ Queue_Array.java
@@ -0,0 +1,61 @@
+public class Queue_Array {
+    static class Queue {
+        int arr[];
+        int size;
+        int rear;
+
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        public boolean isEmpty() {
+            return rear == -1;
+        }
+
+        public void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Queue is full");
+                return;
+            }
+            rear = rear + 1;
+            arr[rear] = data;
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            // shift elements left
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}

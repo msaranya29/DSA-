97042158efe271f548b2ed7c361c14c13d1fd413
msaranya29 <smalways123@gmail.com>
2025-08-19 22:20:40 +0530

    W

---
+++ QueueReversal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class QueueReversal {
+    public static void reverseQueue(Queue<Integer> queue) {
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()) {
+            stack.push(queue.remove());
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        System.out.println("Original Queue: " + queue);
+        reverseQueue(queue);
+        System.out.println("Reversed Queue: " + queue);
+    }
+}
